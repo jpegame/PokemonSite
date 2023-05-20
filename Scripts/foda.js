b6077a59e2fcee22af274b28c3b789d308506e08
João Paulo Andrade Raimundo <62303009+jpegame@users.noreply.github.com>
2023-05-20 16:53:50 -0300

    salve

--- Scripts/foda.js
+++ Scripts/foda.js
@@ -12,6 +12,12 @@ function GetURLParameter(sParam)
     }
 }
 
+const pokemon_normal    = document.getElementById('pokemon_normal');
+const pokemon_shiny     = document.getElementById('pokemon_shiny');
+const gif_normal        = document.getElementById('gif_normal');
+const gif_shiny         = document.getElementById('gif_shiny');
+const super_gif         = document.getElementById('super_gif');
+
 pokemonid = GetURLParameter('pokemon')
 requestURL = 'https://pokeapi.co/api/v2/pokemon/' + pokemonid
 
@@ -20,15 +26,19 @@ $.ajax({
     dataType: 'json',
     success: async function(response) {
         // Update the HTML page with the JSON data
-        document.getElementById('nome_pokemon').innerHTML = response.forms[0].name
-        document.getElementById('image_pokemon').src = response.sprites.other['official-artwork'].front_default
+        document.getElementById('nome_pokemon').innerHTML   = response.forms[0].name
+        pokemon_normal.src  = response.sprites.other['official-artwork'].front_default
+        pokemon_shiny.src   = response.sprites.other['official-artwork'].front_shiny
+        gif_normal.src      = response.sprites.versions['generation-v']['black-white'].animated.front_default
+        gif_shiny.src       = response.sprites.versions['generation-v']['black-white'].animated.front_shiny
+        super_gif.src       = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/' + pokemonid + '.gif'
         let i =0;
         for (let tipo of response.types) {
             tipo_pokemon = document.createElement('h2')
             tipo_pokemon.innerHTML = tipo.type.name
             tipo_pokemon.classList.add(tipo.type.name)
             tipo_pokemon.classList.add('padrao_tipo')
-            document.getElementById('descricao').appendChild(tipo_pokemon)
+            document.getElementById('descricao').insertBefore(tipo_pokemon,document.getElementById('descricao').firstChild)
             i++;
         }
     },
@@ -36,3 +46,39 @@ $.ajax({
         console.log(error);
     }
 });
+
+function CheckboxPoke() {
+
+    let shinybox = document.getElementById('Shiny').checked
+    let gifbox = document.getElementById('Gif').checked
+    let supergifbox = document.getElementById('SuperGif').checked
+    ResetPoke()
+
+    switch (true) {
+        case (supergifbox):
+            document.getElementById('Shiny').checked = false
+            document.getElementById('Gif').checked = false
+            super_gif.style = 'display: block;'
+            break;
+        case (shinybox  && !gifbox):
+            pokemon_shiny.style = 'display: block;'
+            break;
+        case (shinybox && gifbox):
+            gif_shiny.style = 'display: block;'
+            break;
+        case (!shinybox && gifbox):
+            gif_normal.style = 'display: block;'
+            break;
+        case (!shinybox && !gifbox):
+            pokemon_normal.style = 'display: block;'
+            break;
+    }
+}
+
+function ResetPoke () {
+    pokemon_normal.style    = 'display: none;'
+    pokemon_shiny.style     = 'display: none;'
+    gif_normal.style        = 'display: none;'
+    gif_shiny.style         = 'display: none;'
+    super_gif.style         = 'display: none;'
+}
